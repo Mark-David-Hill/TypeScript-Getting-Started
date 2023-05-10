92da13d96ca21898ceed164b768468ca177c4200
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2023-05-10 14:12:01 -0600

    Add functions for displaying score

--- app/app.ts
+++ app/app.ts
@@ -1,17 +1,32 @@
 function startGame() {
     // starting a new game
 
-    let playerName: string = 'Mark';
+    let playerName: string | undefined = getInputValue('playername');
     logplayer(playerName);
 
-    let messagesElement = document.getElementById('messages');
-    
-    messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...!!!';
+    postScore(80, playerName); 
     
 }
 
-function logplayer(name: string) {
+function logplayer(name: string = 'MultiMath Player'): void {
     console.log(`New game starting for player: ${name}`)
+}
+
+function getInputValue(elementID: string): string | undefined {
+    
+    const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+
+    if (inputElement.value === '') {
+        return undefined;
+    }
+    else {
+        return inputElement.value;
+    }
+}
+
+function postScore(score: number, playerName: string = 'MultiMath Player'): void {
+    const scoreElement: HTMLElement | null = document.getElementById('postedScores');
+    scoreElement!.innerText = `${score} - ${playerName}`;
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
