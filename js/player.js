8a85152014b4eb94837d4acfa3980d305f97b4c3
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2023-05-22 12:28:39 -0600

    Adjust module settings

---
+++ js/player.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Player = void 0;
+class Player {
+    constructor() {
+        console.log('Creating a new Player. His name is');
+    }
+    formatName() {
+        return this.name.toUpperCase();
+    }
+}
+exports.Player = Player;
+//# sourceMappingURL=player.js.map
